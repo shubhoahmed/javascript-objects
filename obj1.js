4ff7d74546baebe3d96176b172b75ec0ee13fce9
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-09 21:43:34 +0600

    call function in a objects

---
+++ obj1.js
@@ -0,0 +1,8 @@
+let user = {
+    userName: 'shubho ahmed',
+    password: 12345,
+    login: function () {
+        console.log('Congratulation, You are log in successfully');
+    }
+}
+user.login();
--- objects.js
+++ objects.js
@@ -3,5 +3,24 @@ let user = {
     name: 'Shubho Ahmed',
     age: 24,
     email: 'shabc@gmail.com',
-    blog: 'this is a blog'
+    blog: 'this is a blog',
+    login: function () {
+        console.log('log in successful.')
+    }
 }
+user.login();
+
+// show js as output
+console.log(user);
+// show a single property value from objects
+let seeSingle = user.age;
+let anoterWay = user['blog'];
+// console.log(seeSingle, anoterWay);
+
+// change a property value from objects
+user.age = 25;
+user['age'] = 50;
+console.log(user.age);
+//using type
+console.log(typeof user);
+console.log(user['blog']);
